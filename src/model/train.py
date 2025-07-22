2e2cb9ddf9f87796c801cc7c9927e7c9ca27bc8a
Alix Bourboulon <alix.bourboulon@gmail.com>
2025-07-22 17:25:22 +0200

    data processing done => test tomorrow

--- src/model/train.py
+++ src/model/train.py
@@ -11,18 +11,20 @@ from torch.utils.data import DataLoader
 
 def data_loader(train_folder_path: str, batch_size: int =32) -> DataLoader:
     """
-        Creates a dataloader to convert raw images into tensors.
+        Creates a dataloader to load and convert raw images into tensors.
+        All tensors are normalized in the process.
         input: path of the images folder and size of a computed images batch.
-        output:
+        output: dataloader
     """
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
-    ])
+    ]) # cnserver les mean et std pour la phase de prediction => valeurs classiques pour l' imagerie
     
     train_dataset = ImageFolder(root=train_folder_path, transform=transform)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    
     return train_loader
 
 
